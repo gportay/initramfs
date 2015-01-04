cf2bd205427d7ea355d258086e6dd465778fc534
Gaël PORTAY <gael.portay@gmail.com>
2015-01-04 01:21:38 +0100

    submodules: add kconfig-frontends.
    Signed-off-by: Gaël PORTAY <gael.portay@gmail.com>

---
+++ kconfig-frontends.mk
@@ -0,0 +1,26 @@
+.PHONY:: kconfig-frontends
+
+kconfig-frontends/Makefile: kconfig-frontends/configure
+	( cd $(@D) && ./configure --prefix=/ --enable-config-prefix=CONFIG_ --enable-frontends=conf,mconf,nconf LDFLAGS= )
+	( cd $(@D) && make )
+
+kconfig-frontends/configure: kconfig-frontends/configure.ac
+	( cd kconfig-frontends && autoreconf -vif )
+
+kconfig-frontends_%:
+	make -C kconfig-frontends $*
+
+kconfig-frontends:: kconfig-frontends_all
+
+kconfig-frontends_cleanall::
+	-make -f Makefile kconfig-frontends_clean
+
+kconfig-frontends_mrproper::
+	-make -f Makefile kconfig-frontends_distclean
+	rm -f kconfig-frontends/configure
+
+reallyclean::
+	-make -f Makefile kconfig-frontends_cleanall
+
+mrproper::
+	-make -f Makefile kconfig-frontends_mrproper
