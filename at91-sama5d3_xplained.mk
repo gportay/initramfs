3b13c55344411c59ec94a17ca8507f36eb03b2a0
Gaël PORTAY <gael.portay@gmail.com>
2015-01-19 22:24:30 +0100

    overlay: init: fix ugly persistent typo.
    Signed-off-by: Gaël PORTAY <gael.portay@gmail.com>

--- at91-sama5d3_xplained.mk
+++ at91-sama5d3_xplained.mk
@@ -7,7 +7,7 @@ ifeq (y,$(CONFIG_RC_LOCAL))
 tgz-$(CONFIG_NETWORKING)	+= install-at91-sama5d3_xplained/networking.tgz
 endif
 
-tgz-$(CONFIG_OVERLAY_FS)	+= install-at91/persistant.tgz
+tgz-$(CONFIG_OVERLAY_FS)	+= install-at91/persistent.tgz
 tgz-$(CONFIG_OVERLAY_FS)	+= install-initramfs/overlay.tgz
 
 install-at91-sama5d3_xplained/%.tgz:
