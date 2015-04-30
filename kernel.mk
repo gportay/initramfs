f008c802e35792a007dcd5a436128015b5686bd1
Gaël PORTAY <gael.portay@gmail.com>
2015-04-30 22:02:25 +0200

    Makefile: move kernel stuff in a chunk makefile.
    Signed-off-by: Gaël PORTAY <gael.portay@gmail.com>

---
+++ kernel.mk
@@ -0,0 +1,27 @@
+%.dtb: linux/arch/$(arch)/boot/dts/%.dts
+	@echo "Building $@ for $(ARCH)..."
+	make -C linux $@
+	cp linux/arch/$(arch)/boot/dts/$@ .
+
+linux_%s:
+	make -C linux $*
+
+linux/.config:
+	@echo "You need to provide your own kernel sources into the ./linux directory!"
+	@echo "Have a look at https://www.kernel.org!"
+	@exit 1
+
+linux/arch/$(arch)/boot/$(IMAGE): initramfs.cpio linux/.config
+	@echo "Building $(IMAGE) for $(ARCH)..."
+	make -C linux $(@F) CONFIG_INITRAMFS_SOURCE=../$<
+
+$(IMAGE): linux/arch/$(arch)/boot/$(IMAGE)
+	cp linux/arch/$(arch)/boot/$@ $@
+
+kernel: $(IMAGE)
+
+clean::
+	rm -f $(IMAGE)
+
+mrproper::
+	rm -f *Image *.dtb
