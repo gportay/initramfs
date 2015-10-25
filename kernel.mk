8d31ad12722a098dc8b56c244e66a7e4d8145dd8
Gaël PORTAY <gael.portay@gmail.com>
2015-10-25 23:09:34 +0100

    kernel.mk: allow to fetch latest kernel sources.
    By running:
    $ make linux_download
    
    Signed-off-by: Gaël PORTAY <gael.portay@gmail.com>

--- kernel.mk
+++ kernel.mk
@@ -11,10 +11,15 @@ kernel_% linux_%:
 
 kernel_menuconfig linux_menuconfig:
 
+kernel_download linux_download:
+	wget -qO- https://www.kernel.org/index.html | sed -n '/<td id="latest_link"/,/<\/td>/s,.*<a.*href="\(.*\)">\(.*\)</a>.*,wget -qO- \1 | tar xvJ \&\& ln -sf linux-\2 linux,p' | sh
+
 linux/Makefile:
 	@echo "You need to provide your own kernel sources into the $(CURDIR)/$(@D) directory!" >&2
-	@echo "Have a look at https://www.kernel.org! or run the command below:" >&2
+	@echo "Have a look at https://www.kernel.org! or run one of the commands below:" >&2
 	@echo "$$ git clone git@github.com:torvalds/linux.git $(CURDIR)/$(@D)" >&2
+	@echo "or" >&2
+	@echo "$$ make $(@D)_download" >&2
 	@exit 1
 
 linux/.config: linux/Makefile
