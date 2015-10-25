fa4c981f9bcd43511ee8308afb8217154fb09959
Gaël PORTAY <gael.portay@gmail.com>
2015-10-25 23:03:19 +0100

    kernel.mk: update error messages.
    To provide example of command to run and to print the absolute path to the
    kernel source directory.
    
    Signed-off-by: Gaël PORTAY <gael.portay@gmail.com>

--- kernel.mk
+++ kernel.mk
@@ -12,12 +12,15 @@ kernel_% linux_%:
 kernel_menuconfig linux_menuconfig:
 
 linux/Makefile:
-	@echo "You need to provide your own kernel sources into the ./linux directory!" >&2
-	@echo "Have a look at https://www.kernel.org!" >&2
+	@echo "You need to provide your own kernel sources into the $(CURDIR)/$(@D) directory!" >&2
+	@echo "Have a look at https://www.kernel.org! or run the command below:" >&2
+	@echo "$$ git clone git@github.com:torvalds/linux.git $(CURDIR)/$(@D)" >&2
 	@exit 1
 
 linux/.config: linux/Makefile
 	@echo "You need to configure your kernel using a defconfig file!" >&2
+	@echo "Run the command below:" >&2
+	@echo "$$ make -C $(@D) ARCH=$(karch) menuconfig" >&2
 	@exit 1
 
 linux/arch/$(karch)/boot/$(KIMAGE): initramfs.cpio linux/.config
